72af4d71abe449a46087cb76f1c09403b8cf9d54
jffgrdnr <jeffreywgardner@gmail.com>
2014-03-15 00:50:18 -0600

    socket updates

---
+++ socket/index.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var app = express();
+var port = 3700;
+ 
+app.get("/", function(req, res){
+    res.send("Wassup?");
+});
+
+app.set('views', __dirname + '/tpl');
+app.set('view engine', "jade");
+app.engine('jade', require('jade').__express);
+app.get("/", function(req, res){
+    res.render("page");
+});
+
+app.use(express.static(__dirname + '/public'));
+
+ //app.listen(port);
+var io = require('socket.io').listen(app.listen(port));
+io.sockets.on('connection', function (socket) {
+    socket.emit('message', { message: 'Welcome to Socket Chat' });
+    socket.on('send', function (data) {
+        io.sockets.emit('message', data);
+    });
+});
+
+
+console.log("The Socket app is listening on port " + port);
+
+
