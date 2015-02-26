ae625f578315d1195179f34c8c7c9a0e60496fb9
jffgrdnr <jeffreywgardner@gmail.com>
2015-02-26 14:06:32 -0700

    hackathon code

--- hackathon/js/app.js
+++ hackathon/js/app.js
@@ -3,7 +3,17 @@ var App = angular.module('App', []);
 App.controller('ViewerCtrl', function($scope, $http, $log) {
   $http.get('js/records.json')
        .then(function(res){
-          $scope.records = res.data; 
-          $log(res);               
+          $scope.records = res.data;            
         });
+});
+
+App.directive('fallbackSrc', function () {
+    var fallbackSrc = {
+        link: function postLink(scope, iElement, iAttrs) {
+            iElement.bind('error', function() {
+                angular.element(this).attr("src", iAttrs.fallbackSrc);
+            });
+        }
+    }
+    return fallbackSrc;
 });
