5ccf9a45c6d0f3b566d7cbc945a4c7c1481be05c
jffgrdnr <jegardner@ancestry.com>
2015-07-21 13:23:52 -0600

    adding ligatures to nodes

--- prototype/js/main.js
+++ prototype/js/main.js
@@ -14,13 +14,30 @@ $(document).ready(function(){
 			width: "280px"
 		});
 	});
+	$(".form input").on("click", function(){
+		var thisInput = $(this),
+			thisLabel = thisInput.siblings("label");
+		thisLabel.addClass("show");
+	});
 	$(".form input").keyup(function(){
-		var thisInput = $(this);
+		var thisInput = $(this),
+			thisLabel = thisInput.siblings("label");
+		thisLabel.addClass("show");
 		if ($(thisInput).val().length > 3){
-			thisInput.parent(".ancCol").addClass("icon iconCheck");
+			$(thisInput).blur(function(){
+				thisInput.parent(".ancCol").addClass("icon iconCheck", {duration:800});
+			});
 		};
 		if ($(thisInput).val().length < 3){
 			thisInput.parent(".ancCol").removeClass("icon iconCheck");
+			$(thisInput).blur(function(){
+				if ($(thisInput).val().length < 1){
+					thisLabel.removeClass("show");
+				};
+			});
 		};
 	});
+	$(".closeAdd").click(function(){
+		$.modal.close();
+	})
 });
